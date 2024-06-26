b7156d5a03d657716e77d4ae22171e775b4ecb9d
erik-ferreirra <erikdesouzagf@gmail.com>
2024-06-25 21:28:11 -0300

    add components

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,37 @@
-import { Button } from "./components/ui/button"
+import {
+  Table,
+  TableBody,
+  TableCell,
+  TableHead,
+  TableHeader,
+  TableRow,
+} from "@/components/ui/table"
 
 export function App() {
   return (
     <main>
-      <Button>Teste</Button>
+      <Table>
+        <TableHeader>
+          <TableRow>
+            <TableHead>ID</TableHead>
+            <TableHead>Nome do modelo</TableHead>
+            <TableHead>Tipo de modelo</TableHead>
+          </TableRow>
+        </TableHeader>
+        <TableBody>
+          <TableRow>
+            <TableCell>1</TableCell>
+            <TableCell>Plataforma 1</TableCell>
+            <TableCell>Driver</TableCell>
+          </TableRow>
+
+          <TableRow>
+            <TableCell>2</TableCell>
+            <TableCell>Plataforma 2</TableCell>
+            <TableCell>Driver</TableCell>
+          </TableRow>
+        </TableBody>
+      </Table>
     </main>
   )
 }
